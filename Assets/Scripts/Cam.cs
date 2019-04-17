4241326cc0fc7d54dbaf8329b5493b2586542874
Shany <shanyperezr@gmail.com>
2019-04-17 00:06:13 -0400

    .

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    //Use to get player position.
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        //Move with the player on the X and Z axis.
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent playerNav;
+
+    Animator playerAN;
+
+    //Vector to move the player to.
+    Vector3 moveTo;
+
+    void Start()
+    {
+        //Get the the player NavMeshAgent
+        playerNav = GetComponent<NavMeshAgent>();
+
+        //Get the player animator component;
+        playerAN = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Use the navigaor to move the player to the "moveTo" position;
+        playerNav.SetDestination(moveTo);
+
+        //Getting the keys to control the direction of the movement.
+        //Changing the variable that controls what animation will be running on each key.
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveTo = transform.position + Vector3.forward;
+            //If the player is holding shift the character will run.
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                playerAN.SetInteger("state", 5);
+            }
+            else
+            {
+                playerAN.SetInteger("state", 1);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveTo = transform.position + Vector3.back;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                playerAN.SetInteger("state", 5);
+            }
+            else
+            {
+                playerAN.SetInteger("state", 1);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveTo = transform.position + Vector3.left;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                playerAN.SetInteger("state", 5);
+            }
+            else
+            {
+                playerAN.SetInteger("state", 1);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveTo = transform.position + Vector3.right;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                playerAN.SetInteger("state", 5);
+            }
+            else
+            {
+                playerAN.SetInteger("state", 1);
+            }
+        }
+
+        //If non of the keys are being pressed, stop moving
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            moveTo = transform.position;
+            playerAN.SetInteger("state", 0);
+        }
+    }
+}
