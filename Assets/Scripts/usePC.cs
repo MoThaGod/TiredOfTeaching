dd8a6591cdb058a388c8190566f7082bad1dd1a3
Shany <shanyperezr@gmail.com>
2019-05-07 18:58:54 -0400

    interaction
    added name tag reading, and "can use the pc" Debug.Log to know when this things are detecting the character and are ready to interact with it.

--- Assets/Scripts/usePC.cs
+++ Assets/Scripts/usePC.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class usePC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //bool to check whether or not the computer have been hacked.
+    public bool hacked;
+
+    //Black UI overlay to cover the 3D aspect of the game and focus on minigames
+    public GameObject minigameOverlay;
+
     void Start()
     {
-        
+        //initial hacked state equals false
+        hacked = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Detects when the player enters the pc trigger
+    void OnTriggerEnter(Collider other)
     {
-        
+        //Print can use the pc when the player enters in the collider
+        Debug.Log("can use the pc");
+    }
+
+    //Detects when the player exits the pc trigger
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Can't use the pc no more");
+    }
+    
+    //Detects the player every frame it is still inside the trigger
+    void OnTriggerStay(Collider other)
+    {
+        //Detects if the players press E while inside the collider
+        if (Input.GetKeyDown(KeyCode.E) && !hacked)
+        {
+            Debug.Log("E pressed");
+            //set the overlay to true
+            minigameOverlay.SetActive(true);
+        }
     }
 }
