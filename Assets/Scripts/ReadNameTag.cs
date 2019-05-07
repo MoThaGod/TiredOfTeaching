980347e956b5acd48b99f4a70e8952a71eb54eb9
Shany <shanyperezr@gmail.com>
2019-05-07 13:53:41 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/ReadNameTag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadNameTag : MonoBehaviour
+{
+    //set the prof name in the inspector
+    public string profName;
+
+    //text object to display name
+    public Text displayName;
+
+    public GameObject textBox;
+    public GameObject player;
+
+    void OnTriggerEnter (Collider other)
+    {
+        //Detect when player has entered the trigger
+        if (other.gameObject == player)
+        {
+            Debug.Log("Can Read name");
+            displayName.text = profName;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Detect when player has exit the trigger
+        if (other.gameObject == player)
+        {
+            displayName.text = "";
+        }
+    }
+}
